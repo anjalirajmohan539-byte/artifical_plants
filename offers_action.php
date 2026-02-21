927555cddc953fb50a01b35380269224bffbd48b
Anjali <anjalirajmohan539@gmail.com>
2026-02-21 12:54:20 +0530

    not set offer

--- offers_action.php
+++ offers_action.php
@@ -6,13 +6,33 @@ if(isset($_POST['btn']))
     $offerId  = $_POST['type'];
     $productId = $_POST['proId'];
     $status = $_POST['update'];
+    $btn = $_POST['btn'];
+
+// if($btn == "Add")
+// {
+//     $insert = "INSERT INTO product_offers (ProductId, OfferId)
+//                VALUES ('$proid', '$offerId')";
+//     mysqli_query($conn,$insert);
+// }
+
+ if($btn == "Update")
+{
+    $update = "UPDATE offers 
+               SET Status = $status
+               WHERE Id = $offerId";
+    mysqli_query($conn,$update);
+}
+else
+    {
+        header("location:offers.php?productId=$productId");
+    }
 
     if($offerId != 0)
     {
         $insert = "INSERT INTO product_offers 
                    (ProductId, OfferId, Status) 
                    VALUES 
-                   ('$productId', '$offerId', 1)";
+                   ($productId, $offerId, $status)";
 
         if(mysqli_query($conn, $insert))
         {
@@ -28,4 +48,26 @@ if(isset($_POST['btn']))
         echo "Please Select Offer";
     }
 }
+elseif (isset($_POST['delete'])) 
+{
+    $id = $_POST['offId'];
+    $productid = $_POST['productId'];
+
+    
+    $dupdate = "UPDATE product_offers SET IsDeleted = 1 WHERE Id = $id";
+    var_dump($dupdate);
+
+    $dstatement = mysqli_query($conn, $dupdate);
+
+    if (!$dstatement) 
+        {
+        echo "Error updating";
+    } 
+    else 
+    {
+        header("location:offers.php?productId=$productid");
+        exit();
+    }
+}
+
 ?>
