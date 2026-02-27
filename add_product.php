ab99f2b7857fd7c45157ee09d70fbfee0dfb45bc
Anjali Rajmohan <anjalirajmohan539@gmail.com>
2026-02-27 16:54:49 +0530

    Removed a letter from title of add_product.php

--- add_product.php
+++ add_product.php
@@ -35,7 +35,7 @@ if (isset($_POST['edit']) || isset($_GET['productid'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Add Product - Milon Artificial Plants </title>
+  <title>Add Product - Milon Artificial Plant </title>
   <link rel="stylesheet" href="css/add_product.css">
   <link href="bootstrap/bootstrap.min(css).css" rel="stylesheet"  integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <script src="js/jquery.min.js"></script>
